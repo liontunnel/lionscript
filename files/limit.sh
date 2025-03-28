2e954c60b8c1e9ede92be67b824e98c45e7a9600
LION TUNNEL <mrdoe0435@gmail.com>
2025-03-29 06:52:33 +0700

    Update limit.sh

--- files/limit.sh
+++ files/limit.sh
@@ -8,7 +8,7 @@
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # Gemilangkinasih࿐
 
-REPO="https://https://raw.githubusercontent.com/Baimkuy/Autoscript/GM/"
+REPO="https://https://raw.githubusercontent.com/liontunnel/lionscript/GM/"
 wget -q -O /etc/systemd/system/limitvmess.service "${REPO}files/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limitvless.service "${REPO}files/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limittrojan.service "${REPO}files/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
